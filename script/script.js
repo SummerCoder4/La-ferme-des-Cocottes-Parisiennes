ea1b7cb1f0a77401b75d26052f99c08b0b608cc4
Yannick COUSIN <94236356+yannick-cousin@users.noreply.github.com>
2021-12-09 20:24:36 +0100

    Little Update

--- script/script.js
+++ script/script.js
@@ -21,23 +21,3 @@ btn.addEventListener('click', e => {
         e.target.classList.toggle('active');
         menuDeroule.classList.toggle('deroule');
 })
-
-
-// const darkMode = document.querySelector('.darkMode'); 
-
-// darkMode.addEventListener('click', () => {
-
-//   const body = document.body; 
-
-//   if(body.classList.contains('dark')){
-//     body.classList.add('light')
-//     body.classList.remove('dark')
-//     darkMode.innerHTML = "Nuit"
-
-//   } else if(body.classList.contains('light')){
-//     body.classList.add('dark')
-//     body.classList.remove('light')
-//     darkMode.innerHTML = "Jour"
-
-//   }
-// })
