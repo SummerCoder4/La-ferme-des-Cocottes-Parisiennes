778a158540830c0442be8c8a66f70c75365b6dba
Benedct-Laurain <92828220+Benedct-Laurain@users.noreply.github.com>
2021-12-09 14:48:29 +0100

    darkmode
    ajout dark mode - modif css footer a ne pas prendre en compte !

---
+++ script/darkMode.js
@@ -0,0 +1,25 @@
+const darkMode = document.querySelector('.darkMode') 
+
+const audio = new Audio('img/0440.mp3')
+
+
+darkMode.addEventListener('click', () => {
+
+  const body = document.body; 
+
+  if(body.classList.contains('dark')){
+    body.classList.add('light')
+    body.classList.remove('dark')
+    darkMode.innerHTML = "Nuit"  
+    audio.play()
+  
+  } else if(body.classList.contains('light')){
+    body.classList.add('dark')
+    body.classList.remove('light')
+    darkMode.innerHTML = "Jour"
+ 
+  }
+})
+
+
+
