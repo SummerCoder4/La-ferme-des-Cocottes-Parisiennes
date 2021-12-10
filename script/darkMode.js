38d824904b2223a78b4bb74886c073a17d7b09c3
Benedct-Laurain <92828220+Benedct-Laurain@users.noreply.github.com>
2021-12-10 11:43:18 +0100

    ajout maintien darkmode sur toutes les pages

--- script/darkMode.js
+++ script/darkMode.js
@@ -1,20 +1,34 @@
-const darkMode = document.querySelector('.darkMode') 
-const audio = new Audio('img/0440.mp3')
+const darkMode = document.querySelector('.darkMode'); //stock bouton changing mode 
+const audio = new Audio('img/0440.mp3'); //stock musique light
+const body = document.body; //stock le body
 
 darkMode.addEventListener('click', () => {
 
-  const body = document.body; 
-
-  if(body.classList.contains('dark')){
+  if(body.classList.contains('dark')){ //mode light actif
     body.classList.add('light')
     body.classList.remove('dark')
     darkMode.innerHTML = "Nuit"  
     audio.play()
   
-  } else if(body.classList.contains('light')){
+  } else if(body.classList.contains('light')){ //mode dark actif
     body.classList.add('dark')
     body.classList.remove('light')
     darkMode.innerHTML = "Jour"
- 
+  }
+  
+  if(body.className === "dark"){
+    console.log('Nuit');
+    localStorage.setItem('darkMode', 'enabled')
+  } else {
+    console.log('Jour')
+    localStorage.setItem('darkMode', 'disabled')
   }
 })
+
+if(localStorage.getItem('darkMode') == 'enabled') {
+  console.log('On rentre bien dans la condition')
+  body.classList.add('dark');
+  body.classList.remove('light');
+  darkMode.innerHTML = "Jour";
+}
+
